bb13237d80d2d11c7845058be3361f41424d3cdf
Smita Busar <smitabusar@gmail.com>
2013-12-10 11:51:13 -0800

    structure of games.js set

--- js/game.js
+++ js/game.js
@@ -1,5 +1,12 @@
-var games = [];
+var Games = {};
 
-function addGame(gameID, player,state){
-	var
+Games.InProgress=[];
+
+Games.Add = function(gameID, player1Id){
+	var game={};
+	game.id=gameID;
+	game.Players=[];
+	game.Players.push(player1Id);
+	game.openToJoin=true;
+	Games.InProgress.push(game);
 }
